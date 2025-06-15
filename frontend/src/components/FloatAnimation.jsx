022fcd8cafeaa83236f32c6be1be520de8cd8127
Aman Jaiswal <89749348+Amanj-18@users.noreply.github.com>
2025-06-16 03:15:51 +0530

    feat:added bg animation

---
+++ frontend/src/components/FloatAnimation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/FloatAnimation.css'; 
+
+const FloatAnimation = () => {
+  return (
+     <div className="bubble-animation">
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+      </div>
+  )
+}
+
+export default FloatAnimation
