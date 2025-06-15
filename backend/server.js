a7b51cddcc30f1e56d1fe45c610a76cfd3e9a0e0
Aman Jaiswal <89749348+Amanj-18@users.noreply.github.com>
2025-06-16 03:14:07 +0530

    feat:added cors

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ dotenv.config({ path: './.env' });
 
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import { connectToDB } from './db/connect.js';
 import authRoutes from './routes/auth.routes.js';
@@ -11,16 +12,23 @@ import userRoutes from './routes/user.routes.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  'http://localhost:5173', // Local development
+];
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 app.use(cookieParser()); // Middleware to parse cookies
+app.use(cors({
+  origin: allowedOrigins, // Allow requests from the frontend URL
+  credentials: true, // Allow cookies to be sent with requests
+}));
 
 // AUTH ROUTES
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 
 // USER ROUTES
-app.use("/api/user",userRoutes);
+app.use("/api/user", userRoutes);
 
 
 app.listen(PORT, () => {
