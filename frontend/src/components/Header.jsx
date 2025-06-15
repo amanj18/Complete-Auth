f484ceafe0adf17b8a1c4929b4fe3ded0683a041
Aman Jaiswal <89749348+Amanj-18@users.noreply.github.com>
2025-06-16 03:16:04 +0530

    feat:added header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { FaRegFaceSmileBeam } from "react-icons/fa6";
+import '../styles/Header.css'; 
+import { AppContent } from '../context/AppContext';
+
+const Header = () => {
+    const {userData} = useContext(AppContent);
+    console.log("User Data in Header:", userData);
+  return (
+    <header className="header">
+      <h1 className="header__title">
+        Hey {userData ? userData?.name : 'Developer'} <FaRegFaceSmileBeam className="header__icon" />
+      </h1>
+      <h2 className="header__subtitle">Welcome to our app</h2>
+      <p className="header__description">
+        Discover powerful features to help you build, manage, and grow your applications with ease. Let’s get you started on something amazing!
+      </p>
+      <button className="header__button">Get Started</button>
+    </header>
+  );
+};
+
+export default Header;
