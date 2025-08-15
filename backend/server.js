b4aa6b1536dde73ebf2efcdddb3ca1efdb29e92b
Aman Jaiswal <89749348+Amanj-18@users.noreply.github.com>
2025-08-16 00:23:53 +0530

    h

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-enco
 app.use(cookieParser()); // Middleware to parse cookies
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://complete-auth-jp9tdocig-amanjais160-8974s-projects.vercel.app',
+  'https://complete-auth-phi.vercel.app',
 ];
 
 app.use(cors({
