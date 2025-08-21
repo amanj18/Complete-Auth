44ca5b90b0b986bf0b1d0c58359642ec4736d591
Aman Jaiswal <89749348+Amanj-18@users.noreply.github.com>
2025-08-21 23:59:45 +0530

    fix:improved css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,30 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { FaRegFaceSmileBeam } from "react-icons/fa6";
-import '../styles/Header.css'; 
-import { AppContent } from '../context/AppContext';
-import { useNavigate } from 'react-router-dom';
+import "../styles/Header.css";
+import { AppContent } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-    const {userData} = useContext(AppContent);
-    const navigate = useNavigate();
+  const { userData, isLoggedIn } = useContext(AppContent);
+  const navigate = useNavigate();
 
-    const handleChatNavigation = () => {
-      navigate('/chat');
+  const handleChatNavigation = () => {
+    if (!isLoggedIn) {
+      navigate("/login");
+    } else {
+      navigate("/chat");
     }
+  };
 
   return (
     <header className="header">
       <h1 className="header__title">
-        Hey {userData ? userData?.name : 'Developer'}
+        Hey {userData ? userData?.name : "Developer"}
         {userData?.profilePic ? (
-          <img src={userData?.profilePic} alt="User Avatar" className="header__avatar" />
-        ) : (<FaRegFaceSmileBeam className="header__icon" />)}
+          <img
+            src={userData?.profilePic}
+            alt="User Avatar"
+            className="header__avatar"
+          />
+        ) : (
+          <FaRegFaceSmileBeam className="header__icon" />
+        )}
       </h1>
       <h2 className="header__subtitle">Welcome to our app</h2>
       <p className="header__description">
-        Discover powerful features to help you build, manage, and grow your applications with ease. Let’s get you started on something amazing!
+        Discover powerful features to help you build, manage, and grow your
+        applications with ease. Let’s get you started on something amazing!
       </p>
-      <button className="header__button" onClick={handleChatNavigation}>Start Chat</button>
+      <button className="header__button" onClick={handleChatNavigation}>
+        {isLoggedIn ? "Start Chat" : "Login to Chat"}
+      </button>
     </header>
   );
 };
